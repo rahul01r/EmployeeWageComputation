3b235e4fe1a0b7c031713228fb6a4178c44d3c51
rahul01r <rrdubeynibm@gmail.com>
2022-10-21 23:18:10 +0530

    [rahul] Add:UC1-Attendance

---
+++ EmployeeWage/EmployeeRoll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeRoll
+    {
+        public void Attendance()
+        {
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           EmployeeRoll employeeRoll = new EmployeeRoll();
+            employeeRoll.Attendance();
+
+        }
+    }
+}
